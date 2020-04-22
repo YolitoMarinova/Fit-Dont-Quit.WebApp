8be19685c6133d0291b6b4fc05c41fab34a60f33
Yoana <marinova.m.y@gmail.com>
2020-04-22 22:05:50 +0300

    Fix UI

--- Web/FitDontQuit.Web.ViewModels/Trainers/CreateTrainerModel.cs
+++ Web/FitDontQuit.Web.ViewModels/Trainers/CreateTrainerModel.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
 
     using FitDontQuit.Services.Mapping;
@@ -15,30 +16,36 @@
     {
         [Required]
         [MaxLength(NameMaxLenght)]
+        [DisplayName("First name")]
         public string FirstName { get; set; }
 
         [Required]
         [MaxLength(NameMaxLenght)]
+        [DisplayName("Last name")]
         public string LastName { get; set; }
 
         [Range(MinimumAge, MaximumAge)]
+        [DisplayName("Age")]
         public int Age { get; set; }
 
         [Required]
         [MaxLength(DescriptionMaxLenght)]
+        [DisplayName("Description")]
         public string Description { get; set; }
 
         [Required]
-
         [StringLength(MaxPhoneLenght)]
+        [DisplayName("Phone number")]
         public string PhoneNumber { get; set; }
 
+        [DisplayName("Instagram profile")]
         public string InstagramUrl { get; set; }
 
         public string ImageUrl { get; set; }
 
         public IFormFile Image { get; set; }
 
+        [DisplayName("Profession")]
         public int ProfessionId { get; set; }
 
         public IEnumerable<TrainerProfessionsViewModel> Professions { get; set; }
